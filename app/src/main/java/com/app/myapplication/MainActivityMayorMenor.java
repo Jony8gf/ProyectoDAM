5ca8fb529e8bc922fcf9b354cfab67e8c4a6fc70
Jonathan <jgonzalezf09@educantabria.es>
2021-04-29 20:00:22 +0200

    Mayor-Menor-Igual

--- app/src/main/java/com/app/myapplication/MainActivityMayorMenor.java
+++ app/src/main/java/com/app/myapplication/MainActivityMayorMenor.java
@@ -5,18 +5,233 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivityMayorMenor extends AppCompatActivity {
+
+    private TextView tvTragos, tvGuia;
+    private Button btMenor, btMayor, btIgual;
+    private ImageView ivCarta;
+    private int tragos, random, auxRandom, id;
+    private boolean checkMenor, checkMayor, checkIgual;
+    private MediaPlayer mpCorrecto, mpIncorrecto, mpJoker;
+    private String auxiliar;
+
+    private String cartas[] =  new String[]{
+            "carta_joker","carta_as", "carta_dos", "carta_tres", "carta_cuatro", "carta_cinco", "carta_seis", "carta_siete", "carta_ocho", "carta_nueve", "carta_diez", "carta_j","carta_q","carta_k"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_mayor_menor);
 
+        //Inicializacion de variables y asignacion con parte grafica
+        tragos = 1;
+        checkIgual = false;
+        checkMenor = false;
+        checkMayor = false;
 
+        //Textviews
+        tvTragos = findViewById(R.id.textViewContadorTragosMayorMenor);
+        tvGuia = findViewById(R.id.textViewGuiaMayorMenor);
+
+        //Botones
+        btMenor = findViewById(R.id.buttonMenor);
+        btMayor = findViewById(R.id.buttonMayor);
+        btIgual = findViewById(R.id.buttonIgual);
+
+        //ImageView
+        ivCarta = findViewById(R.id.imageViewCartaMayorMenor);
+
+
+        //Asignacion de Sonido
+        mpCorrecto = MediaPlayer.create(this, R.raw.correcto);
+        mpIncorrecto = MediaPlayer.create(this, R.raw.incorrecto);
+        mpJoker = MediaPlayer.create(this, R.raw.joker);
+
+
+        //Generar primera carta random
+        random = (int)(Math.random()*14);
+        if(random == 0){
+            random = 6;
+        }
+
+        //Cambio de Imagen según numero aleatorio & denegar acceso a botón
+        id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+        ivCarta.setImageResource(id);
+
+
+    }
+
+
+    //Metodo Recursivo - Mayor-Menor-Igual
+    public void ComprobacionCarta(){
+
+        auxRandom = random;
+
+        if(tragos==0){
+            //Generar carta random
+            random = (int)(Math.random()*12+1);
+            id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+            ivCarta.setImageResource(id);
+            tragos++;
+            auxiliar = (String) getText(R.string.tragos_acumulados);
+            tvTragos.setText(auxiliar+" " + tragos);
+            auxiliar = (String) getText(R.string.siguiente_carta);
+            tvGuia.setText(auxiliar);
+            checkMenor=false;
+            checkMayor=false;
+            checkIgual=false;
+        }
+
+        if(checkMayor){
+            //Generar carta random
+            random = (int)(Math.random()*14);
+            //Controlador de carta JOKER = 0
+            if(random == 0){
+                tragos++;
+                id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                ivCarta.setImageResource(id);
+                auxiliar = (String) getText(R.string.mandas_bebes);
+                tvTragos.setText(auxiliar+""+tragos);
+                auxiliar = (String) getText(R.string.joker);
+                tvGuia.setText(auxiliar);
+                tragos = 0;
+            }
+            //Controlador MAYOR QUE
+            else {
+                if (random > auxRandom) {
+                    tragos++;
+                    auxiliar = (String) getText(R.string.tragos_acumulados);
+                    tvTragos.setText(auxiliar+" " + tragos);
+                    auxiliar = (String) getText(R.string.acertaste);
+                    tvGuia.setText(auxiliar);
+                    mpCorrecto.start();
+                    id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                    ivCarta.setImageResource(id);
+                } else {
+                    tragos++;
+                    auxiliar = (String) getText(R.string.tragos_acumulados);
+                    tvTragos.setText(auxiliar+" " + tragos);
+                    auxiliar = (String) getText(R.string.fallaste);
+                    tvGuia.setText(auxiliar);
+                    mpIncorrecto.start();
+                    id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                    ivCarta.setImageResource(id);
+                    tragos = 1;
+                }
+            }
+            //Restaurar Check para siguiente carta
+            checkMayor = false;
+        }
+        if(checkMenor){
+            //Generar carta random
+            random = (int)(Math.random()*14);
+            //Controlador de carta JOKER = 0
+            if(random == 0){
+                tragos++;
+                id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                ivCarta.setImageResource(id);
+                auxiliar = (String) getText(R.string.mandas_bebes);
+                tvTragos.setText(auxiliar+""+tragos);
+                auxiliar = (String) getText(R.string.joker);
+                tvGuia.setText(auxiliar);
+                mpJoker.start();
+                tragos = 0;
+            }
+            //Controlador MENOR QUE
+            else {
+                if (random < auxRandom) {
+                    tragos++;
+                    auxiliar = (String) getText(R.string.tragos_acumulados);
+                    tvTragos.setText(auxiliar+" " + tragos);
+                    auxiliar = (String) getText(R.string.acertaste);
+                    tvGuia.setText(auxiliar);
+                    mpCorrecto.start();
+                    id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                    ivCarta.setImageResource(id);
+                } else {
+                    tragos++;
+                    auxiliar = (String) getText(R.string.tragos_acumulados);
+                    tvTragos.setText(auxiliar+" " + tragos);
+                    auxiliar = (String) getText(R.string.fallaste);
+                    tvGuia.setText(auxiliar);
+                    mpIncorrecto.start();
+                    id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                    ivCarta.setImageResource(id);
+                    tragos = 1;
+                }
+            }
+            //Restaurar Check para siguiente carta
+            checkMenor = false;
+        }
+        if(checkIgual){
+            //Generar carta random
+            random = (int)(Math.random()*14);
+            //Controlador de carta JOKER = 0
+            if(random == 0){
+                tragos++;
+                id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                ivCarta.setImageResource(id);
+                auxiliar = (String) getText(R.string.mandas_bebes);
+                tvTragos.setText(auxiliar+""+tragos);
+                auxiliar = (String) getText(R.string.joker);
+                tvGuia.setText(auxiliar);
+                mpJoker.start();
+                tragos = 0;
+            }
+            //Controlador IGUAL QUE
+            else {
+                if (random == auxRandom) {
+                    tragos++;
+                    auxiliar = (String) getText(R.string.tragos_acumulados);
+                    tvTragos.setText(auxiliar+" " + tragos);
+                    auxiliar = (String) getText(R.string.acertaste);
+                    tvGuia.setText(auxiliar);
+                    mpCorrecto.start();
+                    id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                    ivCarta.setImageResource(id);
+                } else {
+                    tragos++;
+                    auxiliar = (String) getText(R.string.bebes);
+                    tvTragos.setText(auxiliar +" "+tragos);
+                    auxiliar = (String) getText(R.string.fallaste);
+                    tvGuia.setText(auxiliar);
+                    mpIncorrecto.start();
+                    id = getResources().getIdentifier(cartas[random], "drawable", getPackageName());
+                    ivCarta.setImageResource(id);
+                    tragos = 1;
+                }
+            }
+            //Restaurar Check para siguiente carta
+            checkIgual = false;
+        }
+    }
+
+
+    //BOTÓN MAYOR QUE
+    public void mayorQue(View view){
+        checkMayor = true;
+        ComprobacionCarta();
+    }
+
+    //BOTÓN MENOR QUE
+    public void menorQue(View view){
+        checkMenor = true;
+        ComprobacionCarta();
+    }
+    //BOTÓN IGUAL QUE
+    public void igualQue(View view){
+        checkIgual = true;
+        ComprobacionCarta();
     }
 
 
